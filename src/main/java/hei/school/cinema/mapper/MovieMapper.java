ae17124bed90869840979c9adf19e1fc0050928e
Ambinintsoa007 <hei.ambinintsoa@gmail.com>
2026-08-08 19:09:13 +0300

    refactor(movies): use manual DTOs in movie mapper

--- src/main/java/hei/school/cinema/mapper/MovieMapper.java
+++ src/main/java/hei/school/cinema/mapper/MovieMapper.java
@@ -1,6 +1,7 @@
 package hei.school.cinema.mapper;
 
-import hei.school.cinema.gen.model.MovieResponse;
+import hei.school.cinema.endpoint.rest.dto.Genre;
+import hei.school.cinema.endpoint.rest.dto.MovieResponse;
 import hei.school.cinema.model.Movie;
 import hei.school.cinema.repository.model.GenreEntity;
 import hei.school.cinema.repository.model.MovieEntity;
@@ -14,45 +15,50 @@ public class MovieMapper {
 
   public Movie toDomain(MovieEntity entity) {
     Set<hei.school.cinema.model.Genre> genres =
-        entity.getGenres().stream()
-            .map(mg -> toDomainGenre(mg.getId().getGenre()))
-            .collect(Collectors.toSet());
+            entity.getGenres().stream()
+                    .map(movieGenre -> toDomainGenre(movieGenre.getId().getGenre()))
+                    .collect(Collectors.toSet());
+
     return Movie.builder()
-        .id(entity.getId())
-        .title(entity.getTitle())
-        .description(entity.getDescription())
-        .duration(java.time.Duration.ofSeconds(entity.getDurationSeconds()))
-        .genres(genres)
-        .build();
+            .id(entity.getId())
+            .title(entity.getTitle())
+            .description(entity.getDescription())
+            .duration(java.time.Duration.ofSeconds(entity.getDurationSeconds()))
+            .genres(genres)
+            .build();
   }
 
   public MovieEntity toEntity(Movie movie) {
     MovieEntity entity =
-        MovieEntity.builder()
-            .id(movie.getId())
-            .title(movie.getTitle())
-            .description(movie.getDescription())
-            .durationSeconds(movie.getDuration().toSeconds())
-            .build();
+            MovieEntity.builder()
+                    .id(movie.getId())
+                    .title(movie.getTitle())
+                    .description(movie.getDescription())
+                    .durationSeconds(movie.getDuration().toSeconds())
+                    .build();
+
     entity.setGenres(
-        movie.getGenres().stream()
-            .map(
-                genre ->
-                    MovieGenreEntity.builder()
-                        .id(new MovieGenreEntity.MovieGenreId(entity.getId(), toEntityGenre(genre)))
-                        .movie(entity)
-                        .build())
-            .collect(Collectors.toSet()));
+            movie.getGenres().stream()
+                    .map(
+                            genre ->
+                                    MovieGenreEntity.builder()
+                                            .id(
+                                                    new MovieGenreEntity.MovieGenreId(
+                                                            entity.getId(), toEntityGenre(genre)))
+                                            .movie(entity)
+                                            .build())
+                    .collect(Collectors.toSet()));
+
     return entity;
   }
 
   public MovieResponse toDto(Movie movie) {
     return new MovieResponse(
-        movie.getId(),
-        movie.getTitle(),
-        movie.getDescription(),
-        movie.getDuration().toString(),
-        movie.getGenres().stream().map(this::toDtoGenre).collect(Collectors.toList()));
+            movie.getId(),
+            movie.getTitle(),
+            movie.getDescription(),
+            movie.getDuration().toString(),
+            movie.getGenres().stream().map(this::toDtoGenre).toList());
   }
 
   public hei.school.cinema.model.Genre toDomainGenre(GenreEntity entityGenre) {
@@ -63,16 +69,15 @@ public class MovieMapper {
     return GenreEntity.valueOf(domainGenre.name());
   }
 
-  public hei.school.cinema.gen.model.Genre toDtoGenre(hei.school.cinema.model.Genre domainGenre) {
-    return hei.school.cinema.gen.model.Genre.valueOf(domainGenre.name());
+  public Genre toDtoGenre(hei.school.cinema.model.Genre domainGenre) {
+    return Genre.valueOf(domainGenre.name());
   }
 
-  public hei.school.cinema.model.Genre toDomainGenre(hei.school.cinema.gen.model.Genre dtoGenre) {
-    return hei.school.cinema.model.Genre.valueOf(dtoGenre.getValue());
+  public hei.school.cinema.model.Genre toDomainGenre(Genre dtoGenre) {
+    return hei.school.cinema.model.Genre.valueOf(dtoGenre.name());
   }
 
-  public Set<hei.school.cinema.model.Genre> toDomainGenres(
-      Set<hei.school.cinema.gen.model.Genre> dtoGenres) {
+  public Set<hei.school.cinema.model.Genre> toDomainGenres(Set<Genre> dtoGenres) {
     return dtoGenres.stream().map(this::toDomainGenre).collect(Collectors.toSet());
   }
 }
